25277d69108097ebbee4c9a66de8071dafa74a67
Kevin Postal <kevindpostal@gmail.com>
2010-09-24 19:00:03 +0000

    Added Url checking

--- folder_text_search.py
+++ folder_text_search.py
@@ -6,12 +6,19 @@
 # 2010
 # 
 
-
+#### Kevin Postal ###
+## kevindpostal@gmail.com
+# This code searches a path and all its subdirectories
+# for a string/regular expression then goes to the url
+#
+# 2010
+# 
+from httplib import HTTP
+from urlparse import urlparse
 import fileinput, glob, string, sys, os, re 
 from os.path import join
 
-buffsize = 5242880 * 10 #50 Megabyte buffer limit (Server was not having 
-max size)
+buffsize = 5242880 * 10 #50 Megabyte buffer limit (Server was not having max size)
 search_string = r'select.+phone.+agent' #search string
 
 def search(path):
@@ -19,13 +26,26 @@ def search(path):
         for filename in filenames:
             file_path = os.path.join(dirpath, filename)
             f = open(file_path, 'r')
-            reg_check = re.findall(search_string, 
-f.read(buffsize),re.DOTALL)
+            reg_check = re.findall(search_string, f.read(buffsize),re.DOTALL)
             if ( reg_check ):
-                print file_path
+                file_list = list(file_path.split("/")[5:])
+                file_path = '/'.join(file_list)
+                url = "http://csiprocessing.com/%s" % (file_path)
+                print "%s - %s" % (url,checkURL(url) )
+                
             f.close()    
 
 
+def checkURL(url):
+     p = urlparse(url)
+     h = HTTP(p[1])
+     h.putrequest('HEAD', p[2])
+     h.endheaders()
+     if h.getreply()[0] == 200: return 1
+     else: return 0
+
+            
+            
 def main(argv=sys.argv):
     search( argv[1] )
     
